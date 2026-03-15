ecd5a5467b8089a8fa6cca7e317f4a36c78f22e0
zeron-tokyo <zeron.tokyo@gmail.com>
2026-03-15 23:31:51 +0900

    Refine ZERON logo breathing motion and glow

--- lib/widgets/zeron_logo.dart
+++ lib/widgets/zeron_logo.dart
@@ -25,7 +25,7 @@ class _ZeronLogoState extends State<ZeronLogo>
 
     controller = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 2600),
+      duration: const Duration(milliseconds: 2800),
     )..repeat(reverse: true);
 
     final curved = CurvedAnimation(
@@ -34,12 +34,12 @@ class _ZeronLogoState extends State<ZeronLogo>
     );
 
     scale = Tween<double>(
-      begin: 0.985,
-      end: 1.02,
+      begin: 0.988,
+      end: 1.028,
     ).animate(curved);
 
     opacity = Tween<double>(
-      begin: 0.82,
+      begin: 0.84,
       end: 1.0,
     ).animate(curved);
   }
@@ -53,22 +53,28 @@ class _ZeronLogoState extends State<ZeronLogo>
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
+
     final isMobileLike = screenWidth < 700;
 
     final fontSize = isMobileLike ? screenWidth * 0.105 : 40.0;
+
     final letterSpacing = isMobileLike ? screenWidth * 0.02 : 8.0;
 
     return AnimatedBuilder(
       animation: controller,
       builder: (context, child) {
-        final idleOpacityOffset = widget.isIdle ? -0.18 : 0.0;
+        final idleOpacityOffset = widget.isIdle ? -0.16 : 0.0;
+
         final resolvedOpacity =
         (opacity.value + idleOpacityOffset).clamp(0.0, 1.0);
 
         final t = controller.value * math.pi * 2;
-        final driftX = math.sin(t) * 1.8;
-        final driftY = math.cos(t * 0.8) * 2.4;
-        final idleDriftBoost = widget.isIdle ? 1.35 : 1.0;
+
+        final driftX = math.sin(t) * 2.0;
+
+        final driftY = math.cos(t * 0.85) * 2.6;
+
+        final idleDriftBoost = widget.isIdle ? 1.45 : 1.0;
 
         return Transform.translate(
           offset: Offset(
@@ -94,12 +100,12 @@ class _ZeronLogoState extends State<ZeronLogo>
           fontWeight: FontWeight.w300,
           shadows: const [
             Shadow(
-              color: Color.fromRGBO(255, 255, 255, 0.18),
-              blurRadius: 12,
+              color: Color.fromRGBO(255, 255, 255, 0.22),
+              blurRadius: 14,
             ),
             Shadow(
-              color: Color.fromRGBO(255, 255, 255, 0.08),
-              blurRadius: 24,
+              color: Color.fromRGBO(255, 255, 255, 0.1),
+              blurRadius: 28,
             ),
           ],
         ),
